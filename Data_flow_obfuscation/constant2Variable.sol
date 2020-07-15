cb17741b20061b0324cffb48119aef4e460ddef0
xiaofeng <1506010130@hhu.edu.cn>
2020-07-15 11:28:38 +0800

    update codes

--- Data_flow_obfuscation/constant2Variable.sol
+++ Data_flow_obfuscation/constant2Variable.sol
@@ -4,6 +4,9 @@ pragma solidity 0.6.2;
 
 contract tooManyDigits{
     //In Solidity, scientific counting method is provided to represent big numbers and small numbers, and ether unit is also provided to represent the amount of ether. Trying to write multiple digits in succession with these substitutions, these effectively reduce the chance of making mistakes and make the code easier to understand.
+    uint256[] public _integer_const = [1000000000000000000, 18000000000000];
+    string[] public _string_const = ["xf's testCase"];
+    address payable[] public _address_const = [0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed, 0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed];
     uint256 public oncePrice = 1000000000000000000; //10^18, integer literal
     uint256 public twicePrice = 18000000000000; //10^12, integer literal
     address public checkAddress = 0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed; //address literal
--- Data_flow_obfuscation/staticDataDynamicGenerate.py
+++ Data_flow_obfuscation/staticDataDynamicGenerate.py
@@ -19,16 +19,27 @@ import json
 import sys
 
 ADDRESS_FLAG = "address"
-STRING_FLAG = "string"
-INT_FLAG = "int"
+STRING_FLAG = "literal_string"
+INT_FLAG = "int_const"
 
 CORPUS_PATH = "Corpus.txt"
+
+START_FLAG = 1
+END_FLAG = 2
 
 
 class staticDataDynamicGenerate:
 	def __init__(self, _solContent, _jsonContent):
 		self.content = _solContent
 		self.json = _jsonContent
+		self.corpusDict = self.getCorpus()
+
+	def getCorpus(self):
+		corpusDict = dict()
+		with open(CORPUS_PATH, "r", encoding  = "utf-8") as f:
+			corpusDict = json.loads(f.read())
+		#print(corpusDict,"kkkkkkkk")
+		return corpusDict
 
 	def doGenerate(self):
 		#1. find each literal 
@@ -37,27 +48,47 @@ class staticDataDynamicGenerate:
 		#2.1 declare array to store literal
 		typeList = self.getLiteralType(literalList)
 		nowContent = self.content
-		insertPosition = self.getContractEnd()
-		#print(insertPosition)
+		insertPosition = self.getContractStartOrEnd(END_FLAG)
 		#2.2 write getter function into contract
+		#print(typeList)
 		for _type in typeList:
 			if _type == ADDRESS_FLAG:
-				nowContent = self.insertFunc(nowContent, _type)
+				(nowContent, insertPosition) = self.insertFunc(nowContent, insertPosition, _type)
 			elif _type == STRING_FLAG:
-				nowContent = self.insertFunc(nowContent, _type)
+				(nowContent, insertPosition) = self.insertFunc(nowContent, insertPosition, _type)
 			elif _type == INT_FLAG:
-				nowContent = self.insertFunc(nowContent, _type)
+				(nowContent, insertPosition) = self.insertFunc(nowContent, insertPosition, _type)
+			else:
+				continue	
+		#2.3 insert variable - array
+		for _type in typeList:
+			if _type == ADDRESS_FLAG:
+				pass
+			elif _type == STRING_FLAG:
+				pass
+			elif _type == INT_FLAG:
+				pass
 			else:
 				continue
 
-	def getContractEnd(self):
+
+		print(nowContent)
+
+	def getContractStartOrEnd(self, _flag):
 		_list = self.findLiteral(self.json, "name", "ContractDefinition")
-		contractEnd = list()
+		#contractEnd = list()
 		temp = list()
 		for _dict in _list:
 			temp.append(_dict["src"])
+		if _flag == END_FLAG:
+			return self.listToInt(temp[0].split(":"))
+		elif _flag == START_FLAG:
+			return self.listToInt(temp[0].split(":")[0])
+		'''
 		for item in temp:
-			print(self.listToInt(item.split(":")))
+			contractEnd.append(self.listToInt(item.split(":")))
+		return contractEnd
+		'''
 
 
 	def listToInt(self, _list):
@@ -66,27 +97,23 @@ class staticDataDynamicGenerate:
 			totalSum += int(num)
 		return totalSum
 
-
-
-
-	#TO DO (TOB)
-	def insertFunc(self, _content, _type):
-		corpusDict = dict()
-		with open(CORPUS_PATH, "r", encoding  = "utf-8") as f:
-			corpusDict = json.loads(f.read())
+	def insertFunc(self, _content, _position, _type):
 		intStr = str()
-		for item in corpusDict:
-			if item.get("type") == "getIntFunction" and _type == INT_FLAG:
-				intStr = item.get("functionHeadAndBody")
-			elif item.get("type") == "getStrFunction" and _type == STRING_FLAG:
-				intStr = item.get("functionHeadAndBody")
-			elif item.get("type") == "getAddrFunction" and _type == ADDRESS_FLAG:
-				intStr = item.get("functionHeadAndBody")
+		for item in self.corpusDict:
+			if item == "insertFunc":
+				for _dict in self.corpusDict[item]:
+					if _type == INT_FLAG and _dict.get("type") == "getIntFunction":
+						intStr = _dict.get("functionHeadAndBody")
+					elif  _type == STRING_FLAG and _dict.get("type") == "getStrFunction":
+						intStr = _dict.get("functionHeadAndBody")
+					elif  _type == ADDRESS_FLAG and _dict.get("type") == "getAddrFunction":
+						intStr = _dict.get("functionHeadAndBody")
+		#print(self.strInsert(_content, intStr, _position))#, _position + len(intStr))
+		return self.strInsert(_content, intStr, _position), _position + len(intStr)
 
-
-
-
-
+    #Inserting a substr into another str at specific position
+	def strInsert(self, _oldContent, _insertContent, _position):
+		return _oldContent[:_position - 1] + _insertContent + _oldContent[_position - 1:]
 
 		#self.declareAndInitArray(literalList, self.content, self.json)
 		'''
