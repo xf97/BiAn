6c82c78c8581ffe9d1074150c3fc4da88b053a0d
xiaofeng <1506010130@hhu.edu.cn>
2020-09-11 20:13:46 +0800

    delete some files and add one testcase

---
+++ testCase/testCase8.sol
@@ -0,0 +1,44 @@
+/*
+ * @source: https://smartcontractsecurity.github.io/SWC-registry/docs/SWC-124#arbitrary-location-write-simplesol
+ * @author: Suhabe Bugrara
+ * @vulnerable_at_lines: 27
+ */
+
+ pragma solidity ^0.4.25;
+
+ contract Wallet {
+     uint[] private bonusCodes;
+     address private owner;
+     uint256 public num;
+
+     constructor() public {
+         bonusCodes = new uint[](0);
+         owner = msg.sender;
+	 num = 1;
+     }
+
+     function () public payable {
+     }
+
+     function PushBonusCode(uint c) public {
+         bonusCodes.push(c);
+	 uint num1 = 2;
+	 bonusCodes.push(num1);
+     }
+
+     function PopBonusCode() public {
+         // <yes> <report> ACCESS_CONTROL
+         require(0 <= bonusCodes.length); // this condition is always true since array lengths are unsigned
+         bonusCodes.length--; // an underflow can be caused here
+     }
+
+     function UpdateBonusCodeAt(uint idx, uint c) public {
+         require(idx < bonusCodes.length);
+         bonusCodes[idx] = c; // write to any index less than bonusCodes.length
+     }
+
+     function Destroy() public {
+         require(msg.sender == owner);
+         selfdestruct(msg.sender);
+     }
+ }
